2edc70babbe0b54d2e84c3bd693855e0eb3e2757
km00776 <km00776@surrey.ac.uk>
2018-12-09 22:43:56 +0000

    Succesfully migrated attachment_image

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,6 @@
 class Album < ActiveRecord::Base
   belongs_to :user
 
+  has_attached_file :image, styles: { medium: "400x600#>"}
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181209192027) do
+ActiveRecord::Schema.define(version: 20181209224316) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.string   "artist"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size",    limit: 8
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
