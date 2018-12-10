4c9056d812ec3b2aa2756381093cae7c9def10cc
km00776 <km00776@surrey.ac.uk>
2018-12-10 18:13:09 +0000

    Changed background

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181209224316) do
+ActiveRecord::Schema.define(version: 20181210174223) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "title"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 20181209224316) do
     t.string   "image_content_type"
     t.integer  "image_file_size",    limit: 8
     t.datetime "image_updated_at"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "rating"
+    t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
