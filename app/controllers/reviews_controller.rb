7b25198ce7fcabd2fe02064cc6241c96ff1bb1f7
km00776 <km00776@surrey.ac.uk>
2018-12-10 18:26:01 +0000

    Review controler created

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -26,44 +26,23 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     if @review.save
-      redirect_to @album
+      redirect_to @review
     else
       render 'new'
     end
   end
 
-    respond_to do |format|
-      if @review.save
-        format.html { redirect_to @review, notice: 'Review was successfully created.' }
-        format.json { render :show, status: :created, location: @review }
-      else
-        format.html { render :new }
-        format.json { render json: @review.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 
   # PATCH/PUT /reviews/1
   # PATCH/PUT /reviews/1.json
   def update
-    respond_to do |format|
-      if @review.update(review_params)
-        format.html { redirect_to @review, notice: 'Review was successfully updated.' }
-        format.json { render :show, status: :ok, location: @review }
-      else
-        format.html { render :edit }
-        format.json { render json: @review.errors, status: :unprocessable_entity }
-    end
+    @review.update(review_params)
   end
 
   # DELETE /reviews/1
   # DELETE /reviews/1.json
   def destroy
     @review.destroy
-    respond_to do |format|
-      format.html { redirect_to reviews_url, notice: 'Review was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
   private
